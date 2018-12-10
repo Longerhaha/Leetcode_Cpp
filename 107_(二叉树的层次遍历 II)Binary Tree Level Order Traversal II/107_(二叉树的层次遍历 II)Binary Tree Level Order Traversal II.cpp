1e88b1a1a67a596751a817310655fa21b335b986
longerhaha <longerhaha@outlook.com>
2018-12-10 19:04:21 +0800

    107_(二叉树的层次遍历 II)Binary Tree Level Order Traversal II 1. 添加新方法队列BFS+堆栈代码； 2. 添加新方法队列BFS+堆栈在README.md中的描述； 3. 修改README.md为最新格式的README描述。

--- 107_(二叉树的层次遍历 II)Binary Tree Level Order Traversal II/107_(二叉树的层次遍历 II)Binary Tree Level Order Traversal II.cpp
+++ 107_(二叉树的层次遍历 II)Binary Tree Level Order Traversal II/107_(二叉树的层次遍历 II)Binary Tree Level Order Traversal II.cpp
@@ -7,6 +7,61 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
+class Solution {
+public:
+	//队列+堆栈
+    //队列BFS的同时将数据记录到堆栈中，BFS时右节点优先进队
+    //记录堆栈中需要记录当前层的所有节点数据与节点个数
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> res;
+        stack<int> s;
+        //如果输入为空
+        if (root == NULL) {
+            return res;
+        }
+        //1. 队列做BFS,右子树优先进队
+        queue<TreeNode*> q;
+        q.push(root);
+        while( !q.empty() ){
+            //获取当前层的数目
+            int len = q.size();
+            vector<int> level;
+            TreeNode* tmp;
+            for( int i = 0; i < len; i++ ){
+                //取一节点放入堆栈中
+                tmp = q.front();
+                q.pop();
+                s.push(tmp->val);
+                //当前层所有的节点如果有孩子节点则压入队列
+                //右子树优先入队
+                if(tmp->right){
+                    q.push(tmp->right);
+                }
+                if(tmp->left){
+                    q.push(tmp->left);
+                }
+            }
+            //记录节点的个数
+            s.push(len);
+        }
+        
+        //2. 遍历堆栈记录数据
+        while( !s.empty() ){
+            vector<int> level_traverse;
+            int len = s.top();
+            s.pop();
+            for( int i = 0; i < len; i++ ){
+                level_traverse.push_back(s.top());
+                s.pop();
+            }
+            res.push_back(level_traverse);
+        } 
+        return res;
+    }
+};
+
+/*BFS+反转法
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
@@ -41,4 +96,4 @@ public:
         }
         return res;
     }
-};
+};*/
