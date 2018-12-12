14a06ec0241c7c83a97b2e872650f8aff311a3b2
longerhaha <longerhaha@outlook.com>
2018-12-12 19:12:39 +0800

    108_(将有序数组转换为二叉搜索树)Convert Sorted Array to Binary Search Tree 1. 添加递归法代码; 2. 添加README.md的描述。

--- 108_(将有序数组转换为二叉搜索树)Convert Sorted Array to Binary Search Tree/108_(将有序数组转换为二叉搜索树)Convert Sorted Array to Binary Search Tree.cpp
+++ 108_(将有序数组转换为二叉搜索树)Convert Sorted Array to Binary Search Tree/108_(将有序数组转换为二叉搜索树)Convert Sorted Array to Binary Search Tree.cpp
@@ -8,24 +8,29 @@
  * };
  */
 class Solution {
+    //涉及的概念
+    //vector<int> 可以理解为一个长度可变的 int 数组 
+    //vector<int>::iterator 可以理解为长度可变的 int 数组的指针
+    //TreeNode* root = new TreeNode(val) root 是从堆中生成一个值为 val 的链表节点的指针
+    //root->left 是 root 所指向节点的左子树根节点的指针, root->right 同理
+    
+    typedef vector<int>::iterator iter;
 public:
-    typedef vector<int>::iterator Iter; 
-    TreeNode* sortedArrayToBST_recurse(Iter begin, Iter end){
-        //如果输入的指针一致，则返回空
-        if(begin == end)
-            return nullptr;
-        //找到中间节点
-        Iter mid = begin + (end - begin) / 2;
-        //生成以中间节点为根节点的树
-        TreeNode* root = new TreeNode(*mid);
-        //递归左子树
-        root->left = sortedArrayToBST_recurse(begin, mid);
-        //递归右子树
-        root->right = sortedArrayToBST_recurse(mid + 1, end);
-        //返回根节点
-        return root;
-    }
+    //采用递归的思路
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return sortedArrayToBST_recurse(nums.begin(), nums.end());
+        return sortedArrayToBST_recurse(nums.begin(), nums.size());
     }
+    //采用递归的思路
+    TreeNode* sortedArrayToBST_recurse(iter begin, int len){
+        //边界情况
+        if(len == 0) return nullptr;
+        if(len == 1) return new TreeNode(*begin);
+        //找到根节点，然后左右递归
+        //左子树长度为 len / 2 
+        //右子树长度是 len - len / 2 - 1
+        TreeNode* root = new TreeNode(*(begin + len / 2));
+        root->left = sortedArrayToBST_recurse(begin, len / 2);
+        root->right = sortedArrayToBST_recurse(begin + len / 2 + 1, len - len / 2 - 1);
+        return root;
+    } 
 };
